582947ca27ac343be38608a4a33ee27255c09fda
steph-edu <stephanyeduarda302@gmail.com>
2023-07-05 17:39:36 -0300

    commit pc de robert

--- src/public/index.php
+++ src/public/index.php
@@ -18,6 +18,20 @@ echo 'OI';
 
 ?>
 
+<?php
+
+include('vendor/autoload.php');
+
+$client = new \GuzzleHttp\Client();
+//$response = $client->request('GET', 'https://g1.com.br');
+
+//echo $response->getBody();
+
+?>
+
+<img src="https://robohash.org/robert.png" alt="" class="col-lg-4" >
+
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
